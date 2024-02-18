4caabbe66822acbb3c71e10b48595b43bfe31110
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-02-18 16:27:46 +0600

    seat function working

--- script/paribahan.js
+++ script/paribahan.js
@@ -14,19 +14,33 @@ function hideElementDisplay(){
                 dis.removeAttribute('Disabled')
                 return alert ("You not more ticket purchase") 
               }
+              btn.setAttribute('Disabled',true)
           btn.style.background='#1DD100';
           seatCount=seatCount+1
             setInnerText('seat-count',seatCount);
             totalSeat=totalSeat-1;
             setInnerText('total-seat',totalSeat) 
-        })
-        const perTicketPrice=parseInt(document.getElementById('per-ticket-price'))
-       console.log(perTicketPrice)
-      
-    }
+            // select section 
+            const perTicketPrice=parseInt(document.getElementById('per-ticket-price').innerText)
+            // console.log(perTicketPrice)
+            const btnText= btn.innerText;
+            //  console.log(btnText,perTicketPrice)
+            const li=document.getElementById('create-section');
+            const selectContainer=document.getElementById('select-container')
+            const p=document.createElement('p');
+            p.innerText=btnText +" "+" Economy "+ perTicketPrice;
+            li.appendChild(p);
 
+            let totalTicketPrice=parseInt(document.getElementById('total-ticket-price').innerText)
+           
+            totalTicketPrice=totalTicketPrice+perTicketPrice;
+            setInnerText('total-ticket-price',totalTicketPrice)
+        })
+       
+    }
+   
     function setInnerText(id,value){
         document.getElementById(id).innerText=value
     }
     
-    btn.setAttribute('Disabled')
+    // btn.setAttribute('Disabled')
